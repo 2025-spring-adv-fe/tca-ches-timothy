a8159d641f8dec7b29f100078f8e712aed2820d2
Timothy Yeung <kazumaotosensei@gmail.com>
2025-04-15 14:02:47 -0500

    Placedholder for total game count

--- src/Play.tsx
+++ src/Play.tsx
@@ -10,7 +10,7 @@ export const Play = () => {
         <h3
           className="text-2xl font-bold"
         >
-          Play
+          Play (0 games played)
         </h3>
         <h4
           className="text-lg font-semibold"
